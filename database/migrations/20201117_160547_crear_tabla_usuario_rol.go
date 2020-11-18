2c2264bc8ca50670f9ce03e28fca84c5fa079bb1
jjvargass <jotavargas89@gmail.com>
2020-11-18 08:28:16 -0500

    feat:migracon

--- database/migrations/20201117_160547_crear_tabla_usuario_rol.go
+++ database/migrations/20201117_160547_crear_tabla_usuario_rol.go
@@ -44,5 +44,18 @@ func (m *CrearTablaUsuarioRol_20201117_160547) Up() {
 // Reverse the migrations
 func (m *CrearTablaUsuarioRol_20201117_160547) Down() {
 	// use m.SQL("DROP TABLE ...") to reverse schema update
+	file, err := ioutil.ReadFile("../scripts/20201117_160547_crear_tabla_usuario_rol_down.sql")
 
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+	}
+
+	requests := strings.Split(string(file), ";")
+
+	for _, request := range requests {
+		fmt.Println(request)
+		m.SQL(request)
+		// do whatever you need with result and error
+	}
 }
