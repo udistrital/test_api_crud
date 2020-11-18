529ef5f0e349118f79e0aaf67fcec6f3ce9b7ed4
jjvargass <jotavargas89@gmail.com>
2020-11-18 08:33:21 -0500

    fix: se elimina owner del sql

--- database/scripts/20201117_160547_crear_tabla_usuario_rol_up.sql
+++ database/scripts/20201117_160547_crear_tabla_usuario_rol_up.sql
@@ -5,8 +5,6 @@ CREATE TABLE public.usuario (
 	CONSTRAINT pk_usuario PRIMARY KEY (id)
 
 );
-ALTER TABLE public.usuario OWNER TO postgres;
-
 
 CREATE TABLE public.rol (
 	id serial NOT NULL,
@@ -15,7 +13,6 @@ CREATE TABLE public.rol (
 	CONSTRAINT pk_rol PRIMARY KEY (id)
 
 );
-ALTER TABLE public.rol OWNER TO postgres;
 
 ALTER TABLE public.rol ADD CONSTRAINT fk_rol_usuario FOREIGN KEY (usuario_id)
 REFERENCES public.usuario (id) MATCH FULL
